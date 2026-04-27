797084a4841c46f8a6bbf58c7a6c0437e78f6903
Dylan <dylanmckay8939@gmail.com>
2026-04-26 21:51:32 -0400

    Add Vercel URL to allow_origins

--- backend/app/main.py
+++ backend/app/main.py
@@ -96,7 +96,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173"],
+    allow_origins=["http://localhost:5173", "https://veil-phi.vercel.app/"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
