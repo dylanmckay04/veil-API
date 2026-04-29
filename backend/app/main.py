0b2b7d9c0214311b47fda02b49a4664a39f34f25
Dylan <dylanmckay2004@gmail.com>
2026-04-29 15:04:22 -0400

    Remove trailing slash from Vercel link to fix 400 errors

--- backend/app/main.py
+++ backend/app/main.py
@@ -146,7 +146,7 @@ app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:5173", "https://static-eight-sepia.vercel.app/"],
+    allow_origins=["http://localhost:5173", "https://static-eight-sepia.vercel.app"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
